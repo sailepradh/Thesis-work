7e720cef975107fd6e99f2a436f841344fc5b220
Saile <sailepradh@users.noreply.github.com>
2016-12-01 00:35:41 +0100

    Update Some_extra_R_codes.R

--- Some_extra_R_codes.R
+++ Some_extra_R_codes.R
@@ -87,3 +87,103 @@ while (i <= 10) {
   }
   i <- i + 1
 }
+
+
+###################################3##########################
+## 1) practising for loops in 2 ways
+
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Loop version 1
+for (l in linkedin){
+  print(l)
+}
+
+# Loop version 2
+
+for (i in 1:length(linkedin)){
+  print (linkedin[i])
+}
+
+
+# 2)  for loop in list
+# The nyc list is already specified
+nyc <- list(pop = 8405837, 
+            boroughs = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"), 
+            capital = FALSE)
+
+# Loop version 1
+for (i in nyc){
+  print(i)
+}
+
+
+# Loop version 2
+for (i in 1:length(nyc)){
+  print(nyc[[i]])
+}
+
+## 3 Using nested loop with matrix
+
+# The tic-tac-toe matrix ttt has already been defined for you
+
+ttt <- some matrix
+# define the double for loop
+for (i in 1:nrow(ttt)) {
+  for (j in 1:ncol(ttt)) {
+    print(paste("On row", i, "and column", j, "the board contains", ttt[i,j]))
+  }
+}
+
+## 4 Importang information about break and next
+## The break statement abandons the active loop: the remaining code in the loop is skipped and the loop is not iterated over anymore.
+## The next statement skips the remainder of the code in the loop, but continues the iteration.
+
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Extend the for loop
+for (li in linkedin) {
+  if (li > 10) {
+    print("You're popular!")
+  } else {
+    print("Be more visible!")
+  }
+  # Add if statement with break
+  if (li > 16){
+    print ("This is ridiculous, I'm outta here!")
+    break
+  }
+  # Add if statement with next
+  if (li < 5){
+    print ("This is too embarrassing!")
+    next
+  }
+  print(li)
+}
+
+
+## 5 Building R loop from scratch
+# Pre-defined variables
+
+rquote <- "r's internals are irrefutably intriguing"
+chars <- strsplit(rquote, split = "")[[1]]
+
+# Initialize rcount
+rcount <- 0
+
+# Finish the for loop
+for (char in chars) {
+  if (char =="r"){
+    rcount = rcount + 1
+  }else if (char == "u"){
+    break
+  }
+}
+
+# Print out rcount
+print(rcount)
+
+
+## ENd of intermediate level data camp
