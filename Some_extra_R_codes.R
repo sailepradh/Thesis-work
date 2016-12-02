44c5df3daa504aba5b0ed5e5b7cae0d8883219e3
Saile <sailepradh@users.noreply.github.com>
2016-12-02 01:16:50 +0100

    Update Some_extra_R_codes.R

--- Some_extra_R_codes.R
+++ Some_extra_R_codes.R
@@ -314,3 +314,36 @@ interpret_all(facebook)
 
 
 ### search ()
+
+
+
+
+
+#### lappy function with anomoyous function for example
+
+# Definition of split_low
+pioneers <- c("GAUSS:1777", "BAYES:1702", "PASCAL:1623", "PEARSON:1857")
+split <- strsplit(pioneers, split = ":")
+split_low <- lapply(split, tolower)
+
+# Transform: use anonymous function inside lapply
+
+names <- lapply(split_low, function (x) {x[1]})
+
+years <- lapply(split_low, function (x){x[2]})
+
+## Function with muliple additional arguments
+# Definition of split_low
+pioneers <- c("GAUSS:1777", "BAYES:1702", "PASCAL:1623", "PEARSON:1857")
+split <- strsplit(pioneers, split = ":")
+split_low <- lapply(split, tolower)
+
+# Generic select function
+select_el <- function(x, index) {
+  x[index]
+}
+
+# Use lapply() twice on split_low: names and years
+
+names <- lapply (split_low , select_el, index = 1)
+years <- lapply (split_low , select_el, index = 2)
